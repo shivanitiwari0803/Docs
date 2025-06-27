f31cfa9879347367067c441876480c021e1b6a45
shivani tiwari <shivanitiwari0803@gmail.com>
2025-06-27 20:51:05 +0530

    Initial Vite React Commit

---
+++ src/components/foreground.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Foreground = () => {
+  return (
+    <div> 
+        <div className='fixed top-0 left-0 z-[3] h-full w-full '> 
+            <div className='w-60 h-80 bg-sky-200 rounded-[20px]'></div> 
+        </div>
+    </div>
+  )
+}
+
+export default Foreground
